48630f92806dfc38819097df79597f52b92a6cd2
sibenlindo <joao.siben@escola.pr.gov.br>
2026-05-11 10:14:23 -0300

    ;

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-10-19T00:00:00");
+const tempoObjetivo1 = new Date("2026-10-19T00:00:00");
 let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
